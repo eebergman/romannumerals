82c7bdcec10d4109084e901e4099b75d30c0d3d3
Erin Bergman <erin.e.bergman@gmail.com>
2016-12-04 01:19:01 -0500

    Added 1000

--- src/main/java/com/ArabicRoman/TranslateToRoman.java
+++ src/main/java/com/ArabicRoman/TranslateToRoman.java
@@ -15,8 +15,8 @@ public class TranslateToRoman {
 	private static Scanner sc = new Scanner(System.in);
 	private static int userNumber;
 	private static String userNumberAsString;
-	private ArrayList<Character> arrayListUserNumber = new ArrayList<>();
-	private ArrayList<Character> romanAnswer = new ArrayList<>();
+	private static StringBuilder romanAsStringBuilder = new StringBuilder();
+	private static ArrayList<Character> romanAnswer = new ArrayList<>();
 
 	// ADD VALIDATION
 	public static void toRoman() {
@@ -29,37 +29,52 @@ public class TranslateToRoman {
 		lengthOfUserNumber = userNumberAsString.length();
 
 		do {
-			if (lengthOfUserNumber > 5 || lengthOfUserNumber < 1) {
+			if (userNumber > 2999 || userNumber < 1) {
 				System.out.println("Invalid");
+				userNumber = 0;
 
 			} else {
 				switch (lengthOfUserNumber) {
 				case 4:
 					mille();
 					break;
+
 				case 3:
-					
+					centum();
+					break;
+
+				case 2:
+					decem();
+					break;
+
+				case 1:
+					unum();
+					break;
+
 				default:
 					break;
 				}
 			}
 
 		} while (userNumber > 0);
+		
+		System.out.println(romanAsStringBuilder);
 
 	}
 
-	private static int mille() {
-		return 0;
+	private static void mille() {
+		romanAsStringBuilder.append('M');
+		userNumber -= 1000;
 	}
-	
+
 	private static int centum() {
 		return 0;
 	}
-	
+
 	private static int decem() {
 		return 0;
 	}
-	
+
 	private static int unum() {
 		return 0;
 	}
