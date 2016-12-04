f0011147e57c4a031750777630159deca242f97c
Erin Bergman <erin.e.bergman@gmail.com>
2016-12-04 00:44:37 -0500

    Stubbed in methods

--- src/main/java/com/ArabicRoman/TranslateToRoman.java
+++ src/main/java/com/ArabicRoman/TranslateToRoman.java
@@ -11,46 +11,57 @@ import java.util.Scanner;
  *
  */
 public class TranslateToRoman {
-	
+
 	private static Scanner sc = new Scanner(System.in);
 	private static int userNumber;
 	private static String userNumberAsString;
 	private ArrayList<Character> arrayListUserNumber = new ArrayList<>();
 	private ArrayList<Character> romanAnswer = new ArrayList<>();
-	
-	//ADD VALIDATION
-	public static void toRoman() {	
-		
+
+	// ADD VALIDATION
+	public static void toRoman() {
+
 		int lengthOfUserNumber;
-		
+
 		System.out.println("Please enter the number to be converted.");
-		
+
 		userNumber = Integer.parseInt(userNumberAsString = sc.nextLine());
 		lengthOfUserNumber = userNumberAsString.length();
 
 		do {
-			
-			switch (lengthOfUserNumber) {
-			case value:
-				
-				break;
+			if (lengthOfUserNumber > 5 || lengthOfUserNumber < 1) {
+				System.out.println("Invalid");
 
-			default:
-				break;
+			} else {
+				switch (lengthOfUserNumber) {
+				case 4:
+					mille();
+					break;
+				case 3:
+					
+				default:
+					break;
+				}
 			}
-			
-			
+
 		} while (userNumber > 0);
-		
+
+	}
+
+	private static int mille() {
+		return 0;
 	}
 	
+	private static int centum() {
+		return 0;
+	}
 	
-	public static int mille(int number) {
-		
-		
-		
-		return number;
-		
+	private static int decem() {
+		return 0;
+	}
+	
+	private static int unum() {
+		return 0;
 	}
 
 }
